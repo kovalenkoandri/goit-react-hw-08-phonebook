976a56bbde44bc3ee65862bae9a1deb8c0b7f674
kovalenkoandri <kovalenkoandrii@gmail.com>
2022-11-23 14:50:36 +0200

    task to contacts

--- src/pages/Tasks.js
+++ src/pages/Tasks.js
@@ -1,15 +1,22 @@
 import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { Helmet } from 'react-helmet';
-import { TaskList } from 'components/TaskList/TaskList';
-import { TaskEditor } from 'components/TaskEditor/TaskEditor';
-import { fetchTasks } from 'redux/tasks/operations';
-import { selectLoading } from 'redux/tasks/selectors';
-
+// import { TaskList } from 'components/TaskList/TaskList';
+// import { TaskEditor } from 'components/TaskEditor/TaskEditor';
+// import { fetchTasks } from 'redux/tasks/operations';
+// import { selectLoading } from 'redux/tasks/selectors';
+import css from './Tasks.module.css';
+import ContactList from 'components/ContactList';
+import ContactForm from 'components/ContactForm';
+import Filter from 'components/Filter';
+import { fetchTasks } from 'redux/operations';
+import { selectLoading, selectError, selectItems } from 'redux/selectors';
 export default function Tasks() {
   const dispatch = useDispatch();
+  console.log(useSelector(selectItems));
+  const items = useSelector(selectItems);
   const isLoading = useSelector(selectLoading);
-
+  const error = useSelector(selectError);
   useEffect(() => {
     dispatch(fetchTasks());
   }, [dispatch]);
@@ -19,9 +26,17 @@ export default function Tasks() {
       <Helmet>
         <title>Your tasks</title>
       </Helmet>
-      <TaskEditor />
-      <div>{isLoading && 'Request in progress...'}</div>
-      <TaskList />
+      {/* <TaskEditor /> */}
+      {/* <div>{isLoading && 'Request in progress...'}</div> */}
+      {/* <TaskList /> */}
+      <h1 className={css.title}>Phonebook</h1>
+      <ContactForm />
+      <h2 className={css.title}>Contacts</h2>
+      <Filter />
+      <ContactList />
+      {isLoading && <b>Loading tasks...</b>}
+      {error && <b>{error}</b>}
+      <p>{items.length > 0 && JSON.stringify(items)}</p>
     </>
   );
 }
--- src/redux/tasks/operations.js
+++ src/redux/tasks/operations.js
@@ -1,12 +1,12 @@
 import { createAsyncThunk } from '@reduxjs/toolkit';
 import axios from 'axios';
-
+axios.defaults.baseURL = 'https://connections-api.herokuapp.com/';
 // GET @ /tasks
 export const fetchTasks = createAsyncThunk(
   'tasks/fetchAll',
   async (_, thunkAPI) => {
     try {
-      const res = await axios.get('/tasks');
+      const res = await axios.get('/contacts');
       return res.data;
     } catch (error) {
       return thunkAPI.rejectWithValue(error.message);
@@ -19,7 +19,7 @@ export const addTask = createAsyncThunk(
   'tasks/addTask',
   async (text, thunkAPI) => {
     try {
-      const response = await axios.post('/tasks', { text });
+      const response = await axios.post('/contacts', { text });
       return response.data;
     } catch (e) {
       return thunkAPI.rejectWithValue(e.message);
@@ -32,7 +32,7 @@ export const deleteTask = createAsyncThunk(
   'tasks/deleteTask',
   async (taskId, thunkAPI) => {
     try {
-      const response = await axios.delete(`/tasks/${taskId}`);
+      const response = await axios.delete(`/contacts/${taskId}`);
       return response.data;
     } catch (e) {
       return thunkAPI.rejectWithValue(e.message);
