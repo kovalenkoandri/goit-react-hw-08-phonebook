7b1d2218042da2e1fc03aa298998636ae859e0f7
kovalenkoandri <kovalenkoandrii@gmail.com>
2022-11-28 03:30:28 +0200

    ContactList btn

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -1,9 +1,19 @@
-import css from './ContactList.module.css';
 import { deleteContact } from 'redux/phonebook/operations';
 import { useSelector, useDispatch } from 'react-redux';
 import { selectItems, selectFilter } from 'redux/phonebook/selectors';
 import { DeleteIcon } from '@chakra-ui/icons';
 import { iconStyles } from 'styles/iconStyles';
+import {
+  Button,
+  Table,
+  Thead,
+  Tbody,
+  Tr,
+  Th,
+  Td,
+  TableCaption,
+  TableContainer,
+} from '@chakra-ui/react';
 
 const ContactList = () => {
   const dispatch = useDispatch();
@@ -13,24 +23,40 @@ const ContactList = () => {
     return items.filter(el => el.name.includes(filter));
   };
   return (
-    <ul>
-      {renderContacts().map(element => {
-        const { id, name, number } = element;
-        return (
-          <li key={id} className={css.renderLi}>
-            {`${name}: ${number} `}
-            <button
-              type="button"
-              className={css.renderBtn}
-              onClick={() => dispatch(deleteContact(id))}
-            >
-              Delete
-              <DeleteIcon sx={iconStyles} />
-            </button>
-          </li>
-        );
-      })}
-    </ul>
+    <TableContainer>
+      <Table variant="simple" colorScheme="teal">
+        <TableCaption>Imperial table</TableCaption>
+        <Thead>
+          <Tr>
+            <Th>Name</Th>
+            <Th>Number</Th>
+            <Th>Action</Th>
+          </Tr>
+        </Thead>
+        <Tbody>
+          {renderContacts().map(element => {
+            const { id, name, number } = element;
+            return (
+              <Tr key={id}>
+                <Td>{`${name}`}</Td>
+                <Td>{`${number}`}</Td>
+                <Td>
+                  <Button
+                    onClick={() => dispatch(deleteContact(id))}
+                    colorScheme="teal"
+                    size="xs"
+                    variant="ghost"
+                    rightIcon={<DeleteIcon sx={iconStyles} />}
+                  >
+                    Delete
+                  </Button>
+                </Td>
+              </Tr>
+            );
+          })}
+        </Tbody>
+      </Table>
+    </TableContainer>
   );
 };
 
