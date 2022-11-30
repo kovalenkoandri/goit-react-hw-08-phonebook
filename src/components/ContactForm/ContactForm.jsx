4ed02f6147830861bd73b9fde19575d0fc38a3ad
kovalenkoandri <kovalenkoandrii@gmail.com>
2022-11-30 11:29:22 +0200

    formik

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -1,5 +1,4 @@
 import css from './ContactForm.module.css';
-import { useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { selectItems } from 'redux/phonebook/selectors';
 import { addContact } from 'redux/phonebook/operations';
@@ -8,74 +7,115 @@ import { iconStyles } from 'styles/iconStyles';
 import { Button } from '@chakra-ui/react';
 import { Input } from '@chakra-ui/react';
 import { InputStyles } from 'styles/inputStyles';
+import { Formik } from 'formik';
 
 const ContactForm = () => {
-  const [name, setName] = useState('');
-  const [number, setNumber] = useState('');
-  const handleChangeName = event => setName(event.target.value);
-  const handleChangePhone = event => setNumber(event.target.value);
   const dispatch = useDispatch();
   const items = useSelector(selectItems);
-  const handleSubmit = (name, number, event) => {
-    event.preventDefault(); // except refresh page onSubmit
-    if (
-      items.some(el =>
-        el.name.toLocaleUpperCase().includes(name.toLocaleUpperCase())
-      )
-    ) {
-      alert(`${name} is already in contacts.`);
-      return;
-    }
-    dispatch(addContact({ name, number }));
-    setName('');
-    setNumber('');
-  };
+
   return (
-    <form
-      className={css.phonebookForm}
-      onSubmit={event => handleSubmit(name, number, event)}
-      autoComplete="off"
+    <Formik
+      initialValues={{ name: '', number: '' }}
+      validate={values => {
+        const errors = {};
+
+        if (!values.name) {
+          errors.name = 'Required';
+        } else if (
+          !/[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$/i.test(
+            values.name
+          )
+        ) {
+          errors.name = 'Invalid name';
+        }
+        if (!values.number) {
+          errors.number = 'Required';
+        } else if (
+          !/\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}/i.test(
+            values.number
+          )
+        ) {
+          errors.number = 'Invalid number';
+        }
+
+        return errors;
+      }}
+      onSubmit={(values, formHelpers) => {
+        const { name } = values;
+        const { number } = values;
+        if (
+          items.some(el =>
+            el.name.toLocaleUpperCase().includes(name.toLocaleUpperCase())
+          )
+        ) {
+          alert(`${name} is already in contacts.`);
+          return;
+        }
+        dispatch(addContact({ name, number }));
+        formHelpers.resetForm();
+      }}
     >
-      <label htmlFor="name">Name</label>
-      <EditIcon sx={iconStyles} />
-      <Input
-        type="text"
-        name="name"
-        id="name"
-        pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
-        title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
-        required
-        className={css.inputName}
-        onChange={handleChangeName}
-        placeholder="Enter name!"
-        value={name}
-        sx={InputStyles()}
-      />
-      <label htmlFor="number">Number</label>
-      <PhoneIcon sx={iconStyles} />
-      <Input
-        sx={InputStyles()}
-        type="tel"
-        name="number"
-        pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
-        title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
-        required
-        className={css.inputName}
-        onChange={handleChangePhone}
-        placeholder="Enter number!"
-        value={number}
-      />
-      <Button
-        type="submit"
-        colorScheme="teal"
-        size="md"
-        variant="ghost"
-        rightIcon={<CheckCircleIcon sx={iconStyles} />}
-        className={css.btnAddContact}
-      >
-        Add contact
-      </Button>
-    </form>
+      {({
+        values,
+
+        errors,
+
+        touched,
+
+        handleChange,
+
+        handleSubmit,
+
+        isSubmitting,
+
+        /* and other goodies */
+      }) => (
+        <form onSubmit={handleSubmit} autoComplete="off">
+          <label>
+            Name may contain only letters, apostrophe, dash and spaces. For
+            example Adrian, Jacob Mercer, Charles de Batz de Castelmore
+            d'Artagnan
+            <EditIcon sx={iconStyles} />
+            <Input
+              type="text"
+              name="name"
+              onChange={handleChange}
+              placeholder="type name here"
+              value={values.name}
+              sx={InputStyles()}
+              className={css.inputName}
+            />
+          </label>
+          {errors.name && touched.name && errors.name}
+          <label>
+            Phone number must be digits and can contain spaces, dashes,
+            parentheses and can start with +
+            <PhoneIcon sx={iconStyles} />
+            <Input
+              type="tel"
+              name="number"
+              onChange={handleChange}
+              placeholder="type number here"
+              value={values.number}
+              sx={InputStyles()}
+              className={css.inputName}
+            />
+          </label>
+          {errors.number && touched.number && errors.number}
+          <Button
+            type="submit"
+            disabled={isSubmitting}
+            colorScheme="teal"
+            size="md"
+            variant="ghost"
+            rightIcon={<CheckCircleIcon sx={iconStyles} />}
+            className={css.btnAddContact}
+          >
+            Submit
+          </Button>
+        </form>
+      )}
+    </Formik>
   );
 };
 
